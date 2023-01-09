da2ac714761d23b821881cd1c45c1bdafbd2003c
Nikita Lokhmatov <nikitalochmatov@gmail.com>
2023-01-09 14:33:58 +0300

    Delete layout directory

--- layout/FilmForm1/FilmForm1.props.ts
+++
@@ -1,3 +0,0 @@
-import { DetailedHTMLProps, HTMLAttributes } from 'react';
-
-export interface FilmForm1Props extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> { }
--- layout/FilmForm2/FilmForm2.props.ts
+++
@@ -1,3 +0,0 @@
-import { DetailedHTMLProps, HTMLAttributes } from 'react';
-
-export interface FilmForm2Props extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> { }
